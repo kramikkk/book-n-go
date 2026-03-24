d76b452995000034c19ad2df27ef06f8f89fcc37
Kramik <markexconde31@gmail.com>
2026-03-24 20:09:25 +0800

    fix: remove unsafe-eval from Content-Security-Policy

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline'",
       "style-src 'self' 'unsafe-inline' fonts.googleapis.com",
       "font-src 'self' fonts.gstatic.com",
       "img-src 'self' data: blob: *.supabase.co",
